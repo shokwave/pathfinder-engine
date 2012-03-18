a5cb7072a17ab5da1de693fa7125b71cc194cbc3
scott fowler <scott@contextualsystems.com>
2012-03-19 02:28:27 +1100

    let blocks! like magic

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,21 +2,29 @@ require 'dice'
 
 describe Dice do
   describe 'd6' do
-    before do
-      @dice = Dice.new(6)
-      @rolls = []
-      50.times do
-        @rolls << @dice.roll
-      end
-    end
+    let (:dice) { Dice.new(6) }
+    
     it "should return a number between 1 and 6 inclusive" do
-      @dice.roll.should be_between(1, 6)
+      dice.roll.should be_between(1, 6)
     end
-    it "should return random results" do
-      @rolls.uniq.length.should be > 3
-    end
-    it "shouldn't gives 0s, 7s, or 25s" do
-      @rolls.should_not include 0, 7, 25
+    
+    describe 'multiple rolls' do
+      let (:rolls) do
+        rolls = []
+        50.times do
+          rolls << dice.roll
+        end
+        rolls
+      end
+
+      it "should return random results" do
+        rolls.uniq.length.should be > 3
+      end
+
+      it "shouldn't gives 0s, 7s, or 25s" do
+        rolls.should_not include 0, 7, 25
+      end
+      
     end
   end
 end
