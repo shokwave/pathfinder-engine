ec628077d6b59894864780afefba6301fb99ec45
scott fowler <scott@contextualsystems.com>
2012-03-23 11:31:20 +1100

    amusing rspec error

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,13 +1,18 @@
 class Dice
   def initialize(number)
-    @number = number
+    @sides = number
   end
     
   def roll
-    rand(1..@number)
+    rand(1..@sides)
   end
   
   def roll_many(count=1)
     Array.new(count) { roll }
+  end
+  
+  def roll_many_drop(count=1, drop=1)
+    rolls = roll_many(count).sort.reverse
+    [rolls.first(rolls.size-drop), rolls.last(rolls.size - (rolls.size-drop))]
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,23 +2,24 @@ require 'dice'
 
 describe Dice do
   describe 'd6' do
-    let (:dice) { Dice.new(6) }
+    let (:dice) { Dice.new 6 }
     
     subject { dice }  
     
-    it { should respond_to(:roll) }
-    it { should respond_to(:roll_many) }
+    it { should respond_to :roll }
+    it { should respond_to :roll_many }
+    it { should respond_to :roll_many_drop }
     
     describe "rolling" do
       subject { dice.roll }
       
-      it { should be_between(1, 6) }
-      it { should be_a(Integer) }
+      it { should be_between 1, 6 }
+      it { should be_a Integer }
       
     end
     
     describe 'multiple rolls' do
-      let(:rolls) { dice.roll_many(30) }
+      let(:rolls) { dice.roll_many 30 }
       
       subject { rolls }
       
@@ -26,8 +27,17 @@ describe Dice do
         subject.uniq.size.should be > 4
       end
       
-      its(:size) { should be(30) }
-      it { should_not include(0, 7, 25) }
+      its(:size) { should be 30 }
+      it { should_not include 0, 7, 25 }
+      
+    end
+    
+    describe 'rolling with drops' do
+      describe 'dropping lowest' do
+        let(:droponce) { dice.roll_many_drop 4, 1}
+        
+        its(:size) { should be 2 }
+      end
       
     end
   end
